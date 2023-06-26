4465385896ca88dc75bc80d22a25a13f0557656b
Ako Baloyi <baloyiako44@gmail.com>
2023-06-26 22:52:20 +0200

    A

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class Player1 : MonoBehaviour
 {
